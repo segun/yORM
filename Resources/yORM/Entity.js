cf8acabf2087b17fd1a1f3e61120255da866585a
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2011-08-02 23:44:09 +0100

    We can now do updates. removed the evals, not neccessary. re-arrange source code for easy viewing
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- Resources/yORM/Entity.js
+++ Resources/yORM/Entity.js
@@ -1,31 +1,3 @@
-var print = function(obj) {
-	Ti.API.info(obj);
-};
-var yORMUtils = ( function() {
-	var api = {};
-	api.getValuesFromResultSet = function(rs, fields) {
-		var newEntity = {};
-		newEntity.id = rs.fieldByName("id");
-		for(field in fields) {
-			newEntity[field] = rs.getFieldByName(field);			
-		}
-		return newEntity;
-	};
-	api.find = function(tableName, db, fieldsAndValues) {
-		var findBySQL = "SELECT * FROM " + tableName + " WHERE ";
-		for(fv in fieldsAndValues) {
-			findBySQL += fv + "='" + fieldsAndValues[fv] + "' and ";
-		}
-
-		findBySQL = findBySQL.substr(0, findBySQL.length - 4);
-		print(findBySQL);
-
-		var rs = db.execute(findBySQL);
-		return rs;
-	}
-	return api;
-} ());
-
 var BaseEntity = function(dbName, tableName, fields, otherDefinitions) {
 	this.db = Titanium.Database.open(dbName);
 	this.tableName = tableName;
@@ -39,7 +11,7 @@ var BaseEntity = function(dbName, tableName, fields, otherDefinitions) {
 			break;
 		}
 		this.createSQL += field + " " + fields[field] + ","
-		eval ( '(this.' + field + '= null)' );
+		this[field] = null;
 		this.csv += field + ",";
 	}
 	this.id = null;
@@ -47,10 +19,12 @@ var BaseEntity = function(dbName, tableName, fields, otherDefinitions) {
 	this.csv = this.csv.substr(0, this.csv.length - 1);
 	this.createSQL = this.createSQL.substr(0, this.createSQL.length - 1);
 
-	if(otherDefinitions != null) {
-		this.createSQL += ", " + otherDefinitions
+	if(otherDefinitions !== undefined) {
+		if(otherDefinitions !== null) {
+			this.createSQL += ", " + otherDefinitions
+		}
 	}
-	
+
 	this.createSQL += ")";
 	print(this.createSQL);
 
@@ -59,8 +33,7 @@ var BaseEntity = function(dbName, tableName, fields, otherDefinitions) {
 BaseEntity.prototype.save = function() {
 	var insertSQL = "INSERT INTO " + this.tableName + "(" + this.csv + ") VALUES ("
 	for(field in this.fields) {
-		var s = '(a = this.' + field + ')';
-		eval(s);
+		var a = this[field];
 		insertSQL += "'" + a + "',";
 	}
 
@@ -81,7 +54,7 @@ BaseEntity.prototype.remove = function() {
 	this.db.execute(deleteSQL);
 	this.id = null;
 	for(field in this.fields) {
-		eval ( '(this.' + field + '= null)');
+		this[field] = null;		
 	}
 };
 BaseEntity.prototype.all = function(clauses) {
@@ -115,14 +88,12 @@ BaseEntity.prototype.find = function(id, clauses) {
 	rs.close();
 	return entity;
 };
-
 BaseEntity.prototype.findBy = function(fieldsAndValues) {
 	var rs = yORMUtils.find(this.tableName, this.db, fieldsAndValues);
 	var entity = yORMUtils.getValuesFromResultSet(rs, this.fields);
 	rs.close()
 	return entity;
 };
-
 BaseEntity.prototype.findAllBy = function(fieldsAndValues) {
 	var rslist = yORMUtils.find(this.tableName, this.db, fieldsAndValues);
 	var rsData = [];
@@ -134,3 +105,47 @@ BaseEntity.prototype.findAllBy = function(fieldsAndValues) {
 	rslist.close();
 	return rsData;
 };
+BaseEntity.prototype.raw = function(rawSQL) {
+	print(rawSQL);
+	var rs = this.db.execute(rawSQL);
+	return rs;
+}
+BaseEntity.prototype.update = function() {
+	var fields = this.fields;
+	var updateSQL = "UPDATE " + this.tableName + " SET "
+	for(field in this.fields) {
+		updateSQL += field + " = '" + this[field] + "', "
+	}
+
+	updateSQL = updateSQL.substr(0, updateSQL.length - 2);
+	updateSQL += " WHERE id = " + this.id;
+	print(updateSQL);
+	this.db.execute(updateSQL);
+};
+var print = function(obj) {
+	Ti.API.info(obj);
+};
+var yORMUtils = ( function() {
+	var api = {};
+	api.getValuesFromResultSet = function(rs, fields) {
+		var newEntity = {};
+		newEntity.id = rs.fieldByName("id");
+		for(field in fields) {
+			newEntity[field] = rs.getFieldByName(field);
+		}
+		return newEntity;
+	};
+	api.find = function(tableName, db, fieldsAndValues) {
+		var findBySQL = "SELECT * FROM " + tableName + " WHERE ";
+		for(fv in fieldsAndValues) {
+			findBySQL += fv + "='" + fieldsAndValues[fv] + "' and ";
+		}
+
+		findBySQL = findBySQL.substr(0, findBySQL.length - 4);
+		print(findBySQL);
+
+		var rs = db.execute(findBySQL);
+		return rs;
+	}
+	return api;
+} ());
